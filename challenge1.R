3551cc2b0f61615f42f44501aa8ad966da02a65d
gg <liangguige0118@outlook.com>
2022-02-07 13:50:24 +0100

    update

--- challenge1.R
+++ challenge1.R
@@ -57,7 +57,13 @@ transactional %>%
 #Note: To calculate daily sales consider only the “active days” of a machine to exclude machine failures. For that, divide the number of items sold by a machine by the total number of “distinct” days.
 #Hint: Check function month() to do the date restriction.
 
-
+#2. [2 points] Consider the following plot of the number of items sold per machine and day
+#Let us focus on the different patterns present in the data:
+  #a. Is there a general trend in the number of snacks and drinks as the months progress
+#from January to April? Is it the same for snacks and drinks? Why do you think that
+#might be so?
+#  b. Is there shorter time period trend as well? Is it the same for snacks and drinks? What
+#do you might be the cause?
 daily_active_machines <- transactional %>% 
   filter(month(as.Date(date)) %in%c(1:4) & year(as.Date(date))==2017) %>% 
   left_join(Machines[,c('machine','small_machine')],by='machine') %>%
@@ -78,4 +84,30 @@ ggplot(daily_sales_type, aes(x = daily_sales_type$`as.Date(date)`, y = daily_sal
   geom_line()
 
 
+#3. [2 points] Given the following distribution of average income:
+summary(Machines$income_average)
 
+#a) Are there outliers? How would you treat them? Provide code with your answer
+
+#Answer: Yes obviously exists outliers since the max and mean value are far from median
+
+quantile(Machines$income_average,0.95,na.rm = T)
+
+#b)Can you give three possibilities on how to treat the NA cases? Which option
+#would you choose and why? Provide code with your answer
+#Hint: Take a look at the relation between having NA in the income average 
+#and the average daily items.
+
+
+
+#4.[2 points] According to the following boxplot, what is the median number of
+#hotels in the machine area?
+boxplot(Machines$num_hotels)
+
+#5.[2 points] In this exercise we will build a location score that tells us 
+#what’s the average daily items per machine depending on the location it is placed. 
+#This model could be used to a) Decide in which locations to place new machines 
+#b) Construct a benchmark for each machine: how much should it sell according to its location? 
+#This can be used to detect problems in machines (i.e. illumination, bad placement within a station etc.)
+
+#For that, you will build a linear model to predict machine daily items using the following features:
